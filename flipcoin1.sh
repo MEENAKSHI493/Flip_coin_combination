601b227bf0c78c1118e6ea7f07f06f4a1819f597
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-06-06 15:51:05 +0530

    flip coin head and tails siglet duplet

--- flipcoin1.sh
+++ flipcoin1.sh
@@ -1,11 +1,7 @@
-# display flip coin Singlet, Doublet and Triplet
 
 declare -A Dictionary
 
 loop=0
-headsper=0
-tailsper=0
-sum=0
 coin2=0
 heads2=0
 tails2=0
@@ -14,27 +10,30 @@ while [[ loop -le 10 ]];do
 
 x=$((RANDOM%2))
 y=$((RANDOM%2))
+z=$((RANDOM%2))
 
-
-if [[ $y -eq 0 ]] && [[ $x -eq $y ]];then
-                        echo "doublet                      : HH"
-                        Dictionary[$coin2]="HH"
+if [[ $z -eq 0 ]] && [[ $x -eq $y ]] && [[ $y -eq $z ]];then
+                        echo "triplet                      : HHH"
+                        dictionary[$coin2]="HHH"
    ((coin2++))
    ((heads2++))
-   elif [[ $y -eq 1 ]] && [[ $x -eq $y ]];then
-                        echo "doublet                      : TT"
-                        Dictionary[$coin2]="TT"
+   elif [[ $y -eq 1 ]] && [[ $x -eq $y ]] && [[ $y -eq $z ]];then
+                        echo "triplet                      : TTT"
+                        Dictionary[$coin2]="TTT"
    ((coin2++))
    ((tails2++))
+else 
+echo Other than Triplet
 fi
-   ((loop++))
+
+  ((loop++))
 done
 
-echo Number of doublets -"$coin2"
+echo Number of triplets -"$coin2"
 
-echo heads doublet percentage:
+echo heads triplet percentage:
 			awk "BEGIN {print 100*$heads2/$loop}"
-echo tails doublet percentage:
+echo tails triplet percentage:
 			awk "BEGIN {print 100*$tails2/$loop}"
-echo doublets percentage     :
+echo triplets percentage     :
 			awk "BEGIN {print 100*$coin2/$loop}"
