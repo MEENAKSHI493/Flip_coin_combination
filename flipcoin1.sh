6fc5be95b40263645116b0c613e564feaa65f4f5
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-06-06 15:35:13 +0530

    flip coin combination

--- flipcoin1.sh
+++ flipcoin1.sh
@@ -1,2 +1,10 @@
-echo "flip coin"
+# displays winning percentage of Head or Tail Combination in a Singlet, Doublet and Triplet
+
+x=$((RANDOM%2))
+
+if [[ $x -eq 0 ]];then
+	 echo winner : heads
+else 
+	echo winner : tails
+fi
 
